faf2c937c8a3bd81ebd874bc04a3a4c5eb4bff38
SanghunBruceHam <123874866+SanghunBruceHam@users.noreply.github.com>
2025-06-25 15:07:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 import { createCard } from './components/cardTemplate.js';
 
 async function loadCategory(cat) {
@@ -12,7 +11,10 @@ const listEl = document.getElementById('grant-list');
 buttons.forEach(btn => {
   btn.addEventListener('click', async () => {
     const grants = await loadCategory(btn.dataset.cat);
-    listEl.innerHTML = '';
-    grants.forEach(g => listEl.appendChild(createCard(g)));
+    listEl.innerHTML = ''; // 이전 카드 제거
+    grants.forEach(g => {
+      const card = createCard(g);
+      listEl.appendChild(card);
+    });
   });
 });
